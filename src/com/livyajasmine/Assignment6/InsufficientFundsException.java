8a59a2be64ca0fc5bb08224fd6c025c478abe80e
livyajasmine <livyajasmine140@gmail.com>
2025-08-28 15:20:55 +0530

    Assignment6

---
+++ src/com/livyajasmine/Assignment6/InsufficientFundsException.java
@@ -0,0 +1,9 @@
+package com.livyajasmine.Assignment6;
+
+public class InsufficientFundsException extends Exception {
+	public InsufficientFundsException(String message) {
+		super(message);
+		
+	}
+
+}
---
+++ src/com/livyajasmine/Assignment6/InvalidAmountException.java
@@ -0,0 +1,8 @@
+package com.livyajasmine.Assignment6;
+
+public class InvalidAmountException extends Exception{
+	public InvalidAmountException(String message) {
+		super(message);
+	}
+
+}
